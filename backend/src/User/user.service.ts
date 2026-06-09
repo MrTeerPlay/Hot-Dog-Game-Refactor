819fb3753c0571bdece3b0ae9edac414a3bf8387
MrTeerPlay <andriyvin08@gmail.com>
2026-06-08 23:03:49 -1000

    Add data base for Register and Login Page

--- backend/src/User/user.service.ts
+++ backend/src/User/user.service.ts
@@ -1,16 +1,38 @@
 import { Injectable } from "@nestjs/common";
+import { PrismaService } from "../Prisma/prisma.service";
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
-    async register(username: string, email: string, password: string) {
-        console.log("Зареєстрований користувач: ", username, email, password);
+    constructor(private prisma: PrismaService) {}
 
-        return { message: "Реєстрація пройшла успішно", username, email }
+    async register(username: string, email: string, password: string) {
+        const passwordHash = await bcrypt.hash(password, 10);
+
+        const user = await this.prisma.user.create({
+            data: { username, email, password, passwordHash }
+        })
+
+        console.log("Зареєстрований користувач: ", username, email, password);
+        return { message: "Реєстрація пройшла успішно", username, email };
     }
 
     async login(email: string, password: string) {
-        console.log("Увійшов в систему користувач: ", email, password);
+        const user = await this.prisma.user.findUnique({ where: { email } });
 
-        return { message: "Логін пройшов успішно" }
+        if (!user) 
+        {
+            throw new Error('Користувача не знайдено');
+        }
+
+        const passwordValid = await bcrypt.compare(password, user.passwordHash);
+
+        if (!passwordValid)
+        {
+            throw new Error('Пароль не дійсний');
+        }
+
+        console.log("Увійшов в систему користувач: ", email, password);
+        return { message: "Логін пройшов успішно" };
     }
 }
